9f135817e1796f842db234db400c7a63ccbad6aa
TejasSharma356 <tejassharma3506@gmail.com>
2026-03-09 22:57:54 +0530

    Fix vite chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase warning limit from 500kb to 1000kb
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // split react and react-dom into separate chunk
+        },
+      },
+    },
+  },
 })
